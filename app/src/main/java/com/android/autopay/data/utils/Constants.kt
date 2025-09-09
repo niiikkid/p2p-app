7ff746bf4c876b5e9efce89aa0dc791bc227e077
Niiikkid <niiikkid@gmail.com>
2025-09-10 09:37:23 +1000

    fix

--- app/src/main/java/com/android/autopay/data/utils/Constants.kt
+++ app/src/main/java/com/android/autopay/data/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.android.autopay.data.utils
 
-const val DEFAULT_URL = "http://10.0.2.2:8000/api/sms"
+const val DEFAULT_URL = "http://10.0.2.2:8000/api/app/sms"
 const val CONNECT_URL: String = "http://10.0.2.2:8000/api/app/device/connect"
 const val FOREGROUND_NOTIFICATION_CHANNEL_ID: String = "notification_handler_channel"
 const val FOREGROUND_NOTIFICATION_ID: Int = 1
