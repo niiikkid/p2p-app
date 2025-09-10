05e257a05d12424abaf7ce340cf8f56f26c72561
Niiikkid <niiikkid@gmail.com>
2025-09-10 12:51:02 +1000

    fix

--- app/src/main/java/com/android/autopay/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/android/autopay/presentation/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.android.autopay.presentation.ui.theme
 
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -36,9 +35,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun AutoPayTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val baseColorScheme = when {
